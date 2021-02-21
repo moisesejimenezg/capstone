c701025cdc5fa893720fd3b9ea5fc3c3f6254830
Moises Jimenez <moisesejimenezg@gmail.com>
2021-02-21 19:18:31 +0100

    Comment broken code

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -70,9 +70,9 @@ class DBWNode(object):
         )
 
         # TODO: Subscribe to all the topics you need to
-        rospy.Subscribe("vehcile/dbw_enabled", Bool, self.dbw_enabled_cb)
-        rospy.Subscribe("/twist_cmd", TwistStamped, self.twist_cmd_cb)
-        rospy.Subscribe("current_velocity", TwistStamped, self.current_velocity_cb)
+        # rospy.Subscriber("vehicle/dbw_enabled", Bool, self.dbw_enabled_cb)
+        # rospy.Subscriber("/twist_cmd", TwistStamped, self.twist_cmd_cb)
+        # rospy.Subscriber("current_velocity", TwistStamped, self.current_velocity_cb)
 
         self.current_velocity = None
         self.current_angle_velocity = None
