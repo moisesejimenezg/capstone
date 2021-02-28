e8af78e5eb257d9af885ca2df4ef55415b9e9465
Moises Jimenez <moisesejimenezg@gmail.com>
2021-02-28 19:22:42 +0100

    Add label generation

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,10 +1,28 @@
 from styx_msgs.msg import TrafficLight
+import cv2
+
+IMG_PREFIX = "/capstone/images/image_"
+IMG_SUFFIX = ".png"
+LABEL_FILE = "/capstone/images/labels.csv"
 
 
 class TLClassifier(object):
     def __init__(self):
         # TODO load classifier
-        pass
+        self.__image_id = 0
+        self.__image_array = []
+        self.__image_size = (800, 600)
+        self.__file_object = open(LABEL_FILE, "w+")
+
+    def save_image(self, image, label):
+        if self.__image_id < 1000:
+            cv2.imwrite(IMG_PREFIX + str(self.__image_id) + IMG_SUFFIX, image)
+            self.__image_id += 1
+            self.__file_object.write(str(label) + "\n")
+        elif not self.__file_object.closed:
+            self.__file_object.close()
+            return True
+        return False
 
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
